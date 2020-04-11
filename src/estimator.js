2868f7a6f667076e35d053a205797845ede3f820
King-alpha <davidoreal1@gmail.com>
2020-04-11 16:26:27 -0700

    finished challenge 3

--- src/estimator.js
+++ src/estimator.js
@@ -1,59 +1,87 @@
-const normaliseDuration = (periodType, timeToElapse) => {
-  if (periodType === 'weeks') return 7 * timeToElapse;
-  else if (periodType === 'months') return 30 * timeToElapse;
-  else return timeToElapse;
+const normaliseDuration = (data) => {
+  if (data.periodType === 'weeks') return 7 * data.timeToElapse;
+  else if (data.periodType === 'months') return 30 * data.timeToElapse;
+
+  return data.timeToElapse;
 };
 
 function calculateInfectionsByRequestedTime(data, currentlyInfected) {
-  const normalisedTimeToElapse = normaliseDuration(
-    data.periodType,
-    data.timeToElapse
-  );
-  const factor = parseInt(normalisedTimeToElapse / 3, 10);
+  const factor = parseInt(data.timeToElapse / 3, 10);
   return currentlyInfected * 2 ** factor;
 }
 
 const impactEstimator = (data) => {
   const currentlyInfected = data.reportedCases * 10;
+
   const infectionsByRequestedTime = calculateInfectionsByRequestedTime(
     data,
     currentlyInfected
   );
+
   const severeCasesByRequestedTime = 0.15 * infectionsByRequestedTime;
 
   const availableBeds = 0.35 * data.totalHospitalBeds;
   const hospitalBedsByRequestedTime =
     availableBeds - severeCasesByRequestedTime;
 
+  const casesForICUByRequestedTime = 0.05 * infectionsByRequestedTime;
+  const casesForVentilatorsByRequestedTime = 0.02 * infectionsByRequestedTime;
+  const { avgDailyIncomePopulation, avgDailyIncomeInUSD } = data.region;
+  const dollarsInFlight =
+    (infectionsByRequestedTime *
+      avgDailyIncomePopulation *
+      avgDailyIncomeInUSD) /
+    data.timeToElapse;
+
   return {
     currentlyInfected,
     infectionsByRequestedTime,
     severeCasesByRequestedTime,
-    hospitalBedsByRequestedTime
+    hospitalBedsByRequestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
   };
 };
 
 const severeImpactEstimator = (data) => {
   const currentlyInfected = data.reportedCases * 50;
+
   const infectionsByRequestedTime = calculateInfectionsByRequestedTime(
     data,
     currentlyInfected
   );
+
   const severeCasesByRequestedTime = 0.15 * infectionsByRequestedTime;
 
   const availableBeds = 0.35 * data.totalHospitalBeds;
   const hospitalBedsByRequestedTime =
     availableBeds - severeCasesByRequestedTime;
 
+  const casesForICUByRequestedTime = 0.05 * infectionsByRequestedTime;
+  const casesForVentilatorsByRequestedTime = 0.02 * infectionsByRequestedTime;
+  const { avgDailyIncomePopulation, avgDailyIncomeInUSD } = data.region;
+  const dollarsInFlight =
+    (infectionsByRequestedTime *
+      avgDailyIncomePopulation *
+      avgDailyIncomeInUSD) /
+    data.timeToElapse;
+
   return {
     currentlyInfected,
     infectionsByRequestedTime,
     severeCasesByRequestedTime,
-    hospitalBedsByRequestedTime
+    hospitalBedsByRequestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
   };
 };
 
 const covid19ImpactEstimator = (data) => {
+  // first normalise the timeToElapse even before starting
+  data.timeToElapse = normaliseDuration(data);
+
   return {
     data,
     impact: impactEstimator(data),
