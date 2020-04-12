8a86b82d78c3cbe503b94f5aa2ecb774446baf86
King-alpha <davidoreal1@gmail.com>
2020-04-11 18:52:22 -0700

    more eslint error fixing

--- src/app.js
+++ src/app.js
@@ -9,9 +9,4 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/v1/on-covid-19', require('./routes'));
 
 const PORT = process.env.PORT || 3000;
-http.createServer(app).listen(PORT, () => {
-  if (process.env.NODE_ENV === 'development') {
-    console.clear();
-    console.log(`server running on ${PORT}`);
-  }
-});
+http.createServer(app).listen(PORT);
