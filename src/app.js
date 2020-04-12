3b83d329c4542127ec9f5634fb65d4568403aec9
King-alpha <davidoreal1@gmail.com>
2020-04-11 19:09:55 -0700

    last fix on eslint error

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
-const http = require('http');
 const express = require('express');
+const http = require('http');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/v1/on-covid-19', require('./routes'));
+app.use('/api/v1/on-covid-19', routes);
 
 const PORT = process.env.PORT || 3000;
 http.createServer(app).listen(PORT);
